f86cb9ec66207cb23d13d739aa7928243eff1ce7
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-02 20:45:12 +0200

    asynchronous request completed

--- main.py
+++ main.py
@@ -1,88 +1,36 @@
-import aiohttp
 import asyncio
+import aiohttp
 from datetime import datetime, timedelta
 
-class PrivatBankAPI:
-    BASE_URL = "https://api.privatbank.ua/p24api/exchange_rates"
+BASE_URL = "https://api.privatbank.ua/p24api/exchange_rates"
 
-    @staticmethod
-    async def get_exchange_rate(session, currency, date):
-        params = {"json": "", "date": date.strftime("%d.%m.%Y")}
-        async with session.get(f"{PrivatBankAPI.BASE_URL}?exchange&coursid=5", params=params) as response:
-            data = await response.json()
-            rates = data.get("exchangeRate")
-            for rate in rates:
-                if rate["currency"] == currency:
-                    return rate["purchaseRate"], rate["saleRate"]
-        return None
+
+async def fetch_exchange_rate(session, fetch_date):
+    async with session.get(f"{BASE_URL}?date={fetch_date}") as response:
+        text_json = await response.text()
+        return text_json
+
 
 async def fetch_exchange_rates():
-    currencies = ["USD", "EUR"]
+    days_for_rate = 5
     end_date = datetime.now()
-    start_date = end_date - timedelta(days=3)
+    start_date = end_date - timedelta(days=days_for_rate - 1)
+    print(start_date.strftime("%d.%m.%Y"), end_date.strftime("%d.%m.%Y"))
 
     async with aiohttp.ClientSession() as session:
-        for currency in currencies:
-            print(f"Exchange rates for {currency} in the last 10 days:")
-            current_date = start_date
-            while current_date <= end_date:
-                try:
-                    purchase_rate, sale_rate = await PrivatBankAPI.get_exchange_rate(session, currency, current_date)
-                    print(f"{current_date.strftime('%Y-%m-%d')}: Purchase Rate - {purchase_rate}, Sale Rate - {sale_rate}")
-                except Exception as e:
-                    print(f"Error fetching exchange rates for {currency} on {current_date.strftime('%Y-%m-%d')}: {e}")
+        tasks = []
+        current_date = start_date
+        while current_date <= end_date:
+            tasks.append(fetch_exchange_rate(session, current_date.strftime("%d.%m.%Y")))
+            current_date += timedelta(days=1)
 
-                current_date += timedelta(days=1)
+        res = await asyncio.gather(*tasks)
+
+    print(res)
+
 
 async def main():
     await fetch_exchange_rates()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
-
-
-
-
-
-# # Version 1
-# import aiohttp
-# import asyncio
-# from datetime import datetime, timedelta
-#
-# class PrivatBankAPI:
-#     BASE_URL = "https://api.privatbank.ua/p24api/exchange_rates"
-#
-#     @staticmethod
-#     async def get_exchange_rate(currency, date):
-#         async with aiohttp.ClientSession() as session:
-#             params = {"json": "", "date": date.strftime("%d.%m.%Y")}
-#             async with session.get(f"{PrivatBankAPI.BASE_URL}?exchange&coursid=5", params=params) as response:
-#                 data = await response.json()
-#                 rates = data.get("exchangeRate")
-#                 for rate in rates:
-#                     if rate["currency"] == currency:
-#                         return rate["purchaseRateNB"], rate["saleRateNB"]
-#         return None
-#
-# async def fetch_exchange_rates():
-#     currencies = ["USD", "EUR"]
-#     end_date = datetime.now()
-#     start_date = end_date - timedelta(days=10)
-#
-#     for currency in currencies:
-#         print(f"Exchange rates for {currency} in the last 10 days:")
-#         current_date = start_date
-#         while current_date <= end_date:
-#             try:
-#                 purchase_rate, sale_rate = await PrivatBankAPI.get_exchange_rate(currency, current_date)
-#                 print(f"{current_date.strftime('%Y-%m-%d')}: Purchase Rate - {purchase_rate}, Sale Rate - {sale_rate}")
-#             except Exception as e:
-#                 print(f"Error fetching exchange rates for {currency} on {current_date.strftime('%Y-%m-%d')}: {e}")
-#
-#             current_date += timedelta(days=1)
-#
-# async def main():
-#     await fetch_exchange_rates()
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
